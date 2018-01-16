d771e709c1dc07da1268ebb14facb82f28b3ef7c
Adam <adotengel@gmail.com>
2018-01-15 20:28:15 -0800

    "Added HelloPi.c to print the first 3 digits of pi as a decimal."

---
+++ HelloPi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    int x = 0;
+    while(x<10) {
+        printf("Not Hello World!\n");
+        if (x < 9){
+            for(int y=0; y<=x; y=y+1) {
+                printf(" ");
+            }
+        }
+        printf("Also... ");
+        printf("%.2f \n", M_PI);
+        x = x + 1;
+    }
+}
